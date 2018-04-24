4aa9bc91d584d1d325707f3cd4bd7c135f873dc3
subhakut <38650977+subhakut@users.noreply.github.com>
2018-04-24 14:04:34 +0530

    Create even.c

---
+++ codekata/even.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main ()
+{
+ int a;
+printf("enter the number:");
+ scanf("\n%d",&a);
+ if(a%2==0)
+    printf("even");
+ else
+    printf("odd");
+  return 0;
+}
