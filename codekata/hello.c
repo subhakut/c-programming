06ba2d6583ccafdac2a2ea21ac92f6fed6564e54
subhakut <38650977+subhakut@users.noreply.github.com>
2018-07-07 07:37:51 +0000

    Create hello.c

---
+++ codekata/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int i;
+    scanf("%d",&a);
+    printf("output\n");
+    for(i=1;i<=a;i++)
+    {
+        printf("hello\t");
+    }
+    return 0;
+}
